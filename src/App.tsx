53cd0d1314b89c91d5da07f737f588ef4ad78d5b
Anna Saushkina <saushkinanna@gmail.com>
2026-03-04 19:15:20 +0300

    refactor: оптимизация файловой системы

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,6 @@
-// import { useEffect } from "react";
-import "./App.css";
-// import { taskStore } from "./state/TaskStore";
 import AppLayout from "./layout/AppLayout";
 
 function App() {
-  // useEffect(() => {
-  //   taskStore.addTask("Test");
-  // }, []);
-
   return <AppLayout />;
 }
 
