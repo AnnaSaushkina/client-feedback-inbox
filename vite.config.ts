0936a1634cc30f56b43496750f4bdd5a64a55d64
Anna Saushkina <saushkinanna@gmail.com>
2026-03-09 18:56:28 +0300

    fix: deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/https://github.com/AnnaSaushkina/client-feedback-inbox/",
+  base: "/client-feedback-inbox/",
   plugins: [react()],
   server: {
     proxy: {
